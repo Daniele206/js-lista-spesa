1a9f966f3e823d424975c6edf4e4201e7e9a93a0
Daniele206 <danieledeieso2004@gmail.com>
2024-02-27 17:05:53 +0100

    [edit] exercise

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,36 +8,36 @@ const shoppingList = [
   'carne',
   'latte',
   'coca-cola',
-]
+];
 
 const takenList = [];
-let validItem = false;
 
 const btn = document.querySelector('.btn');
 const text = document.querySelector('.my_text');
+let listNumber = 0;
 
 btn.addEventListener('click', function(){
   let validItem = false;
   btn.innerHTML = 'Altro oggetto';
 
   while (!validItem){
-    const randomNumber = Math.floor(Math.random() * shoppingList.length);
-
-    const shoppingItem = shoppingList[randomNumber];
+    const shoppingItem = shoppingList[listNumber];
 
     validItem = !takenList.includes(shoppingItem);
 
     if(validItem){
       takenList.push(shoppingItem);
       text.innerHTML = shoppingItem;
-    }
-
-    if(takenList.length === shoppingList.length & !validItem){
+      listNumber = listNumber + 1;
+    };
+    
+    if(takenList.length - 1 === shoppingList.length){
       btn.innerHTML = 'Ricomincia lista';
       text.innerHTML = 'Lista finita';
       
       takenList.splice(0);
+      listNumber = 0;
       validItem = true;
-    }
-  }
-})
+    };
+  };
+});
